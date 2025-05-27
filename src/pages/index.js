927d5ea15e39cb74056091dd0fee8e9581a2ec3d
Hivessy Róbert <robert.hivessy@gmail.com>
2025-05-27 02:28:06 +0200

    fix: update homepage with working GitHub Pages links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,19 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from 'react';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+      title="TechWriting Házi 4"
+      description="Dokumentációs oldal Docusaurusszal, CI/CD-vel">
+      <main style={{ padding: '2rem' }}>
+        <h1>Üdvözöllek!</h1>
+        <p>Itt található a házi feladathoz tartozó dokumentáció:</p>
+        <ul>
+          <li><a href="/hazi04/docs/tutorials/intro">Tutorialok</a></li>
+          <li><a href="/hazi04/docs/guides/installation-guide">Guides</a></li>
+          <li><a href="/hazi04/docs/petstore/list-all-pets">Petstore API</a></li>
+        </ul>
       </main>
     </Layout>
   );
